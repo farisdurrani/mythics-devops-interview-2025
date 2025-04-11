3cc6fdd0e31f351723f8baeaeb6157d812ecaf1b
bclavel <brodyclavel@AMGM416W6KQ.local>
2025-04-11 14:48:23 -0400

    Change VCN name and cidr to match diagram

--- terraform/vcn.tf
+++ terraform/vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
-  cidr_block     = "10.0.0.0/16"
+  cidr_block     = "10.23.0.0/20"
   compartment_id = local.lab_user_cmp
-  display_name   = "Sample VCN"
+  display_name   = "IC_VCN"
 }
