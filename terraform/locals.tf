31be20f1b8a794c19d404bd7c2e136f2f499ae84
bclavel <brodyclavel@AMGM416W6KQ.local>
2025-04-11 14:44:51 -0400

    Add variable for lab copartment

---
+++ terraform/locals.tf
@@ -0,0 +1,3 @@
+locals {
+    lab_user_cmp = "ocid1.compartment.oc1..aaaaaaaa5iou33qre5uczs2ehpa22urfotryd4d4dyycvg54udxclty6owka"
+}
--- terraform/vcn.tf
+++ terraform/vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_vcn" "internal" {
   dns_label      = "internal"
   cidr_block     = "10.0.0.0/16"
-  compartment_id = "" #obtain from interviewer
+  compartment_id = local.lab_user_cmp
   display_name   = "Sample VCN"
 }
