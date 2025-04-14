af2105bacc93943c9d8ef4dd5ca937a37b921d57
bclavel <brodyclavel@AMGM416W6KQ.local>
2025-04-14 12:07:31 -0400

    Add terrafrom locals file and remove uneeded provider file

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,3 +1,10 @@
 locals {
+    #For Resource Placement
     lab_user_cmp = "ocid1.compartment.oc1..aaaaaaaa5iou33qre5uczs2ehpa22urfotryd4d4dyycvg54udxclty6owka"
+
+    #For Compute Instance
+    shape = "VM.Standard.E5.Flex"
+    image = "ocid1.image.oc1.iad.aaaaaaaawoewdiy5y3abewk3byd6456ry5qsphzsm6mjw7jcwnxobs2c7jjq"
+    availability_domain = "ktgl:US-ASHBURN-AD-1"
+    fault_domain = "FAULT-DOMAIN-1"
 }
